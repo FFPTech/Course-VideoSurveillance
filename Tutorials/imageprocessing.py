517cdc232b07bd7b9854f1835b87f720f907c61e
Fit-For-Purpose Technologies <62441881+FFPTech@users.noreply.github.com>
2024-12-12 17:49:59 +0100

    Add display, filtering and masking function to imageprocessing.py

--- Tutorials/imageprocessing.py
+++ Tutorials/imageprocessing.py
@@ -1,29 +1,39 @@
 # Install packages
-# pip install scikit-image, matplotlib
+# pip install scikit-image, matplotlib, numpy
  
 # import libraries
 import skimage as ski # for image processing
 import matplotlib.pyplot as plt # for image visualization
+import numpy as np # for numerical calculations
 
 # check version
 print(ski.__version__)
 
+# Add functions
+# image display
+def displayImage(image, title, colormap='gray', lockwindow=True):
+    plt.figure() # Create figure window
+    plt.imshow(image, cmap=colormap) # Display image on figure with chosen colormap
+    plt.title(title)
+    plt.show(block=lockwindow) # Lock displayed image in figure
+    return 
+
+# image filtering
+def filterImage(image, threshold):
+    image_filtered = image < threshold
+    return image_filtered
+
+# image masking
+def maskImage(image, mask):
+    image_masked = image*mask
+    return image_masked
+
 # load image
 img = ski.data.camera() #load camera image and assign to variable img
-print(type(img))
 
-# filter image using a threshold
-thresh = 75
-img_filtered = img < thresh
-
-# Display loaded image
-plt.figure() # Create figure window
-plt.imshow(img, cmap='gray') # Display image on figure with 'gray' colormap
-plt.title('original image')
-plt.show(block=False) # Show displayed image in figure
-
-# Display loaded image
-plt.figure() # Create figure window
-plt.imshow(img_filtered, cmap='gray') # Display image on figure with 'gray' colormap
-plt.title('filtered image')
-plt.show(block=True) # Show displayed image in figure
+# display image
+displayImage(img, "Original Image")
+img_f = filterImage(img, 75)
+displayImage(img_f, "Filtered Image")
+img_m = maskImage(img, img_f)
+displayImage(img_m, "Masked Image")
